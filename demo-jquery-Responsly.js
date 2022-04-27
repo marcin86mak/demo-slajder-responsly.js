a3e88d8f4d9181947cca12361b85bf49f0a31017
X-plugin.pl <marcin86mak@gmail.com>
2022-04-27 21:01:33 +0200

    go 5

---
+++ demo-jquery-Responsly.js
@@ -0,0 +1,9 @@
+{
+    "infiniteLoopProtection": true,
+    "hardReloadOnChange": true,
+    "view": "browser",
+    "startScript":"go",
+    "node":"14",
+    "port":"1234",
+    "template":"parcel"
+}
